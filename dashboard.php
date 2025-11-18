5f92f922acd504312a4841d3ac2ee293432477a3
nayladelina <nayladelina15@gmail.com>
2025-11-18 22:10:33 +0700

    update

--- dashboard.php
+++ dashboard.php
@@ -1,22 +1,23 @@
 <?php
-session_start 0:
-I/ Cek apakah user sudah login
-if (!isset ($_SESSION['username'])) {
-header ("Location: login.php") :
-exit;
+session_start();
+
+// Cek apakah user sudah login
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
 }
 ?>
-<! DOCTYPE html>
-chtml
+<!DOCTYPE html>
+<html>
 <head>
-<title>Dashboard</title>
+    <title>Dashboard</title>
 </head>
 <body>
-<h2> Selamat datang, <?php echo $_SESSION['username ']: 2>!</h2>
-<?php
-echo "<h2>Sel amat datang,". $_SESSION[ 'username'] . "!</h2> " ;
-2>
-<p>Role: <?php echo $_SESS ION[' role']: ?></p>
-ca href="logout. php">Logout</a>
+
+<h2>Selamat datang, <?php echo $_SESSION['username']; ?>!</h2>
+<p>Role: <?php echo $_SESSION['role']; ?></p>
+
+<a href="logout.php">Logout</a>
+
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,7 +1,11 @@
 <?php
-session_start ():
-I/ Hapus semua session
-session_unset session_destroy () : 0:
-1/ Kemtbali ke halaman login
-header("Location: login . php" ) :
+session_start();      // Memulai session
+
+// Hapus semua session
+session_unset();
+session_destroy();
+
+// Kembali ke halaman login
+header("Location: login.php");
 exit;
+?>
